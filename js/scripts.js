d7bb83c4ac0be352338424feddaf4c8a779b825b
Ananyapam De <ananyapam7@gmail.com>
2020-11-23 15:07:02 +0530

    about

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-    * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
     (function ($) {
     "use strict"; // Start of use strict
 
